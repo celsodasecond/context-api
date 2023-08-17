1db968e6f379676e6b07c477f973caa5a7dc33fc
celsodasecond <75917932+celsodasecond@users.noreply.github.com>
2023-08-17 17:53:33 +0800

    add: button for changing theme

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,7 +4,7 @@ import { ThemeContext } from "../context/ThemeContext";
 class TodoList extends React.Component {
 	static contextType = ThemeContext;
 	render() {
-		const { isDarkTheme, lightTheme, darkTheme } = this.context;
+		const { isDarkTheme, lightTheme, darkTheme, changeTheme } = this.context;
 		const theme = isDarkTheme ? darkTheme : lightTheme;
 
 		return (
@@ -19,6 +19,9 @@ class TodoList extends React.Component {
 				<p className="item">Plan the family trip</p>
 				<p className="item">Go for shopping for dinner</p>
 				<p className="item">Go for a walk</p>
+				<button className="ui button primary" onClick={changeTheme}>
+					Change the Theme
+				</button>
 			</div>
 		);
 	}
