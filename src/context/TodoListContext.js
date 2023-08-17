efc3f17a502f1421a9ed657f8dccde27ef75db69
celsodasecond <75917932+celsodasecond@users.noreply.github.com>
2023-08-17 23:39:55 +0800

    refactor: file structure

--- src/context/TodoListContext.js
+++ src/context/TodoListContext.js
@@ -1,17 +1,7 @@
 import React, { createContext, useState, useReducer } from "react";
+import { todosReducer } from "../reducers/todosReducers";
 
 export const TodoListContext = createContext();
-
-const todosReducer = (state, action) => {
-	switch (action.type) {
-		case "ADD_TODO":
-			return [...state, { text: action.text, id: Math.random() }];
-		case "REMOVE_TODO":
-			return state.filter((todo) => todo.id !== Number(action.id));
-		default:
-			return state;
-	}
-};
 
 const TodoListContextProvider = ({ children }) => {
 	const [todos, dispatch] = useReducer(todosReducer, [
