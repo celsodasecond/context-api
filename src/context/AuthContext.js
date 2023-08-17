3d3ab0dd7d529ddfd9b67e6b7587a07b9641cb86
celsodasecond <75917932+celsodasecond@users.noreply.github.com>
2023-08-17 18:15:38 +0800

    add: Nested/Multiple Contexts

---
+++ src/context/AuthContext.js
@@ -0,0 +1,24 @@
+import React, { createContext } from "react";
+
+export const AuthContext = createContext();
+
+class AuthContextProvider extends React.Component {
+	state = {
+		isLoggedIn: false,
+	};
+
+	changeAuthStatus = () => {
+		this.setState({ isLoggedIn: !this.isLoggedIn });
+	};
+
+	render() {
+		return (
+			<AuthContextProvider
+				value={{ ...this.state, changeAuthStatus: this.changeAuthStatus }}>
+				{this.props.children}
+			</AuthContextProvider>
+		);
+	}
+}
+
+export default AuthContextProvider;
